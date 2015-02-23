682d27afee1a4f9cf934403198ad7ccdadee6dbd
Al <pursuance@gmail.com>
2015-02-23 19:12:48 +0100

    add bookings model and form new + create (still WIP) and rooted the index of flats to home

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!
   def index
+    @flats = Flat.all
+    render :flats(@flats)
   end
 end
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,5 +1,5 @@
 class Flat < ActiveRecord::Base
 
   belongs_to :user
-
+  has_many :bookings
 end
